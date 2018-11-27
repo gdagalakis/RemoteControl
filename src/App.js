e9e2b61cc3f833c9e478c70bc11abcaa431bfd5d
gdagalakis <gdagalakis@gmail.com>
2018-11-27 12:57:48 +0200

    context_provider added for devices and places

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
-import * as R from 'ramda'
 import DeviceForm from './components/DeviceForm'
 import PlaceForm from './components/PlaceForm'
-import { findById } from './lib/utils'
 import 'normalize.css'
 import NavBar from './components/NavBar'
 import Devices from './pages/Devices'
@@ -14,32 +12,14 @@ import { AppDiv } from './style.js'
 import { ThemeProvider } from 'styled-components'
 import 'normalize.css'
 import DeviceProvider from 'lib/DevicesProvider'
-
-const normalizeItem = ({ name, description, posLat, posLong }) => ({
-    name,
-    description,
-    position: { posLat, posLong },
-})
+import PlacesProvider from 'lib/PlacesProvider'
 
 class App extends Component {
     constructor(props) {
         super(props)
-
-        const cachedPlaces = localStorage.getItem('places')
         this.state = {
-            places: JSON.parse(cachedPlaces) || [],
             curTheme: defaultTheme.value,
         }
-    }
-
-    onPlaceFormSubmit = item => {
-        item = normalizeItem(item)
-        item.id = guid()
-        const newPlaces = this.state.places.concat([item])
-        this.setState({
-            places: newPlaces,
-        })
-        localStorage.setItem('places', JSON.stringify(newPlaces))
     }
 
     changeTheme = theme => {
@@ -52,45 +32,42 @@ class App extends Component {
         return (
             <div className="AppDiv">
                 <DeviceProvider>
-                    <Router>
-                        <ThemeProvider theme={this.state.curTheme}>
-                            <AppDiv>
-                                <GlobalStyle />
-                                <NavBar changeTheme={this.changeTheme} />
-                                <Route
-                                    path="/"
-                                    exact
-                                    component={props => (
-                                        <Page title="HomePage">
-                                            <DeviceForm {...props} />
-                                        </Page>
-                                    )}
-                                />
+                    <PlacesProvider>
+                        <Router>
+                            <ThemeProvider theme={this.state.curTheme}>
+                                <AppDiv>
+                                    <GlobalStyle />
+                                    <NavBar changeTheme={this.changeTheme} />
+                                    <Route
+                                        path="/"
+                                        exact
+                                        component={props => (
+                                            <Page title="HomePage">
+                                                <DeviceForm {...props} />
+                                            </Page>
+                                        )}
+                                    />
 
-                                <Route
-                                    path="/devices/"
-                                    component={() => (
-                                        <Page title="Devices">
-                                            <Devices />
-                                        </Page>
-                                    )}
-                                />
-                                <Route
-                                    path="/AddPlaces/"
-                                    component={props => (
-                                        <Page title="AddPlace">
-                                            <PlaceForm
-                                                {...props}
-                                                onSubmit={
-                                                    this.onPlaceFormSubmit
-                                                }
-                                            />
-                                        </Page>
-                                    )}
-                                />
-                            </AppDiv>
-                        </ThemeProvider>
-                    </Router>
+                                    <Route
+                                        path="/devices/"
+                                        component={() => (
+                                            <Page title="Devices">
+                                                <Devices />
+                                            </Page>
+                                        )}
+                                    />
+                                    <Route
+                                        path="/AddPlaces/"
+                                        component={props => (
+                                            <Page title="AddPlace">
+                                                <PlaceForm {...props} />
+                                            </Page>
+                                        )}
+                                    />
+                                </AppDiv>
+                            </ThemeProvider>
+                        </Router>
+                    </PlacesProvider>
                 </DeviceProvider>
             </div>
         )
