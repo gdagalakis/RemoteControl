54155d2f23c7c049aa3cc9a13677b29933859163
gdagalakis <gdagalakis@gmail.com>
2018-11-30 18:21:40 +0200

    ejected

--- src/components/DeviceItem/index.js
+++ src/components/DeviceItem/index.js
@@ -24,11 +24,6 @@ class DeviceItem extends Component {
     }
   }
 
-  toggleEditState = () => {
-    const { editState } = this.state
-    this.setState({ editState: !editState })
-  }
-
   onSave = () => {
     const { saveChanges } = this.props
     const {
@@ -40,6 +35,10 @@ class DeviceItem extends Component {
     const item = { description, ip, name, place }
     saveChanges(item)
     this.toggleEditState()
+  }
+  toggleEditState = () => {
+    const { editState } = this.state
+    this.setState({ editState: !editState })
   }
 
   render() {
